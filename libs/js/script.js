6847430ee6df76e6d9cb8cf752489bf32b8805f6
Sam Welsh <samwelsh@hotmail.co.uk>
2021-02-02 17:12:37 +0000

    First commit

---
+++ libs/js/script.js
@@ -0,0 +1,22 @@
+// Create map
+var mymap = L.map("mapid").setView([51.505, -0.09], 6);
+
+// Map tiles
+var Esri_WorldImagery = L.tileLayer(
+  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+  {
+    attribution:
+      "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+  }
+).addTo(mymap);
+var Stamen_TonerHybrid = L.tileLayer(
+  "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-hybrid/{z}/{x}/{y}{r}.{ext}",
+  {
+    attribution:
+      'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    subdomains: "abcd",
+    minZoom: 0,
+    maxZoom: 20,
+    ext: "png",
+  }
+).addTo(mymap);
