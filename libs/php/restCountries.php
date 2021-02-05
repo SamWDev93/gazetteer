805b5512fd2801b88c8c9b3a526c74b9de6880d9
Sam Welsh <samwelsh@hotmail.co.uk>
2021-02-05 14:09:43 +0000

    Add OpenWeather routine

--- libs/php/restCountries.php
+++ libs/php/restCountries.php
@@ -1,32 +1,58 @@
 <?php
-
+    //RESTCounties Routine
     ini_set('display_errors', 'On');
     error_reporting(E_ALL);
     ini_set('memory_limit', '1024M');
 
     $executionStartTime = microtime(true);
 
-	$url='https://restcountries.eu/rest/v2/alpha/' . $_REQUEST['name'];
+	$rc_url='https://restcountries.eu/rest/v2/alpha/' . $_REQUEST['name'];
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_URL,$url);
+	$rc_ch = curl_init();
+	curl_setopt($rc_ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($rc_ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($rc_ch, CURLOPT_URL, $rc_url);
 
-	$result=curl_exec($ch);
+	$rc_result = curl_exec($rc_ch);
 
-	curl_close($ch);
+	curl_close($rc_ch);
 
-	$decode = json_decode($result,true);	
+	$rc_decode = json_decode($rc_result,true);	
 
-	$output['status']['code'] = "200";
-	$output['status']['name'] = "ok";
-	$output['status']['description'] = "mission saved";
-	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-	$output['data'] = $decode;
+	$rc_output['status']['code'] = "200";
+	$rc_output['status']['name'] = "ok";
+	$rc_output['status']['description'] = "mission saved";
+	$rc_output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+	$rc_output['data'] = $rc_decode;
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
-	echo json_encode($output); 
+    echo json_encode($rc_output);
+    
+    //OpenWeather Routine
+    $ow_api_key = 'cdab949d45e6ad36e58acb23d320ef18';
+
+    $ow_url='https://api.openweathermap.org/data/2.5/onecall?lat=' . $rc_output['data']['latlng'][0] . '&lon=' . $rc_output['data']['latlng'][1] . '&exclude=hourly,minutely&units=metric&appid=' . $ow_api_key;
+
+	$ow_ch = curl_init();
+	curl_setopt($ow_ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ow_ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ow_ch, CURLOPT_URL, $ow_url);
+
+	$ow_result = curl_exec($ow_ch);
+
+	curl_close($ow_ch);
+
+	$ow_decode = json_decode($ow_result,true);	
+
+	$ow_output['status']['code'] = "200";
+	$ow_output['status']['name'] = "ok";
+	$ow_output['status']['description'] = "mission saved";
+	$ow_output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+	$ow_output['data'] = $ow_decode;
+	
+	header('Content-Type: application/json; charset=UTF-8');
+
+    echo json_encode($ow_output);
 
 ?>
